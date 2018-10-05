ecf906021ff0b4e59bdaa55d5bbef79d766782d5
Jeffrey Lipton <jeffreyilipton@gmail.com>
2018-10-05 17:09:04 -0400

    can connect and calibrate and get deltas

--- GelSight/GelSight.m
+++ GelSight/GelSight.m
@@ -1,4 +1,4 @@
-classdef GelSight
+classdef GelSight < handle
     %GELSIGHT Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -18,6 +18,7 @@ classdef GelSight
         
         newDataAvailable 
         stage
+        start_index
         
     end
     
@@ -25,6 +26,7 @@ classdef GelSight
         function obj = GelSight(camNum)
             %GELSIGHT Construct an instance of this class
             %   Detailed explanation goes here
+            imaqreset;
             obj.vid= videoinput('winvideo', camNum, 'RGB24_640x480');
             
             obj.thre_mul1 = 3;
@@ -37,6 +39,7 @@ classdef GelSight
             obj.deltas=[];
             obj.newDataAvailable = false;
             obj.stage = 0;
+            obj.start_index = 0;
         end
         
         function start(obj)
@@ -51,15 +54,16 @@ classdef GelSight
         function calibrate(obj)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
-            obj.calibrationImage = [];
+            im = [];
             avg_num = 2;
             for i = 1:avg_num
                 if isempty(obj.calibrationImage)
-                    obj.calibrationImage = getsnapshot(obj.vid) / avg_num;
+                    im = getsnapshot(obj.vid) / avg_num;
                 else
-                    obj.calibrationImage = obj.calibrationImage + getsnapshot(obj.vid) / avg_num;
+                    im = im + getsnapshot(obj.vid) / avg_num;
                 end
             end
+            obj.calibrationImage = im;
             start(obj.vid);
         end
         
@@ -70,26 +74,28 @@ classdef GelSight
                 [f, time] = getdata(obj.vid, get(obj.vid, 'FramesAvailable'));
                 fend = f(:,:,:,end);
                 pause(0.01);
-                if ~obj.newDataAvailable
-                    stop(obj.vid);
+                del_pic = abs(fend(:) - obj.calibrationImage(:));
+                del_pic(del_pic < 10) = 0;
+                delta = repmat(sum(del_pic), size(time));
+                if isempty(obj.frames)
+                    obj.frames = f;
+                    obj.times = time;
+                    obj.deltas = delta;
+                elseif obj.stage == 0
+                    obj.frames = cat(4, obj.frames(:,:,:,max(end-obj.buffersize, 1):end), f);
+                    obj.times = [obj.times(max(end-obj.buffersize, 1):end); time];
+                    obj.deltas = [obj.deltas(max(end-obj.buffersize, 1):end); delta];
+                else
+                    obj.frames = cat(4, obj.frames, f);
+                    obj.times = [obj.times; time];
+                    obj.deltas = [obj.deltas; delta];
                 end
-            end
-            del_pic = abs(fend(:) - obj.calibrationImage);
-            del_pic(del_pic < 10) = 0;
-            delta = repmat(sum(del_pic), size(time));
-            if isempty(obj.frames)
-                obj.frames = f;
-                obj.times = time;
-                obj.deltas = delta;
-            elseif ob.stage == 0
-                obj.frames = cat(4, obj.frames(:,:,:,max(end-obj.buffersize, 1):end), f);
-                obj.times = [obj.times(max(end-obj.buffersize, 1):end); time];
-                obj.deltas = [obj.deltas(max(end-obj.buffersize, 1):end); delta];
-            else
-                obj.frames = cat(4, obj.frames, f);
-                obj.times = [obj.times; time];
-                obj.deltas = [obj.deltas; delta];
-            test(obj.deltas(end));
+                maxdelta(obj);
+                stageinput(obj);
+                obj.newDataAvailable = true;
+                disp('deltas: ')
+                delta
+                disp(['stage: ',num2str(obj.stage)])
             end
         end        
         
@@ -105,42 +111,68 @@ classdef GelSight
             end
         end
         
+        function stop(obj)
+            stop(obj.vid);
+        end
+        
         function stageinput(obj)
             if obj.stage == 0
                 if obj.deltas(end) > max(obj.init * max(obj.thre_mul1, 2), 1e6)
                     obj.stage = 1;
-                    start_time = obj.times(end);
+                    obj.start_index = length(obj.times);
                     obj.newDataAvailable = true;
                 end
             end
             if obj.stage == 1
                 if obj.deltas(end) < obj.maxd * obj.thre_mul2
                     obj.newDataAvailable = false;
-                    obj.thre_mul1 = obj.maxd / obj.init / 8;
-                    pause(0.01);
-                    
-                    framerate = mean(1./diff(obj.times));
-                    
-                    [~, xmax] = max(obj.deltas);
-                    ann.end = xmax;
-                    savePress(obj.frames, framerate, ann);
-                    
-                    obj.times = obj.times - start_time;
-
+                    obj.times = obj.times - obj.times(obj.start_index);
                     if obj.deltas(end) < obj.init * obj.thre_mul1
-                        obj.stage = 0;
-                        obj.maxd = 0;
-                        obj.calibrationImage = obj.frames(:,:,:,1);
-                        obj.frames = [];
-                        obj.times = [];
-                        obj.deltas = [];
-                        obj.newDataAvailable = false;
-                        flushdata(obj.vid);
+                        obj.stage = 2;
                     end
                 end
             end  
                     
         end
+        
+        function postProcess(obj)
+            name = datestr(datetime('now'), 'mm-dd-yy_HHMMss');
+            folder = 'data'
+            savePress(obj,folder,name);
+        end
+        
+        
+        function clear(obj)
+            obj.maxd = 0;
+            obj.calibrationImage = obj.frames(:,:,:,1);
+            obj.frames = [];
+            obj.times = [];
+            obj.deltas = [];
+            obj.newDataAvailable = false;
+            flushdata(obj.vid);
+            obj.stage=0;
+        end
+        
+        function imgful = savePress(obj,folder,name)
+            [~, xmax] = max(obj.deltas);
+            
+            im = obj.frames(:,:,:,xmax);
+            imgname = [name, '.png'];
+            imgful = [folder, '\', 'image', '\', imgname];
+            imwrite(im, imgful);
+        end
+        
+        function vidname = saveVideo(obj,folder,name)
+            framerate = mean(1./diff(obj.times));
+            [~, xmax] = max(obj.deltas);
+            vidname = [folder,'\','video\', name, '.avi'];
+            v = VideoWriter(vidname);
+            v.FrameRate = framerate;
+            open(v);
+            writeVideo(v, obj.frames(obj.start_index:xmax));
+            close(v);
+        end
+        
         
     end
 end
