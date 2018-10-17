6ae24d7ce74ddc55f12cf22ba71b524ee2fe9026
Jeffrey Lipton <jeffreyilipton@gmail.com>
2018-10-17 17:24:43 -0400

    Update makeWaypoints.m

--- makeWaypoints.m
+++ makeWaypoints.m
@@ -5,7 +5,11 @@ function pts = makeWaypoints(origin,xysize,delta)
     for i = 1:ns(1)
         x = (i-1)*delta+origin(1);
         for j = 1:ns(2)
-            y = (j-1)*delta+origin(2);
+            if mod(i,2)==0
+                y = (j-1)*delta+origin(2);
+            else
+                y = (ns(2)-j)*delta+origin(2);
+            end
             k = (i-1)*ns(2)+(j-1)+1;
             pts(:,k)=[x,y,origin(3),1];
         end
