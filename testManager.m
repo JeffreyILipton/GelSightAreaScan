9d775df0385c8ce4db160b9485343e441432d939
JeffreyILipton <jeffreyilipton@gmail.com>
2018-10-18 15:37:54 -0400

    setup with new PC

--- testManager.m
+++ testManager.m
@@ -3,7 +3,7 @@ clearvars -EXCEPT hwObj
 close all
 clc
 %%
-useHardware = true;
+useHardware = false;
 
 if ~exist('hwObj') && useHardware
     instruct = sprintf([...
@@ -27,11 +27,12 @@ end
 setup = struct();
 %setup.expType = ExpTypes.OptitrackOnly;
 %setup.expType = ExpTypes.GelSightOnly;
-setup.expType = ExpTypes.TestArm;
+%setup.expType = ExpTypes.TestArm;
+setup.expType = ExpTypes.TestHSA;
 setup.expTimeSeconds = 0;%10; %70
 setup.framePeriod = 1;
 setup.offset = [0,0,0];
-setup.camNum=2;
+setup.camNum=1;
 setup.timestep = 0.01;
 setup.debug = false;%true;%
 
@@ -39,7 +40,7 @@ setup.debug = false;%true;%
 setup.HSA_mins = [1300,1450,1360,1300];
 setup.HSA_maxs = [1140,1600,1200,1500];
 setup.HSA_channels = 0:3;
-setup.HSA_port = 'COM4';
+setup.HSA_port = 'COM72';
 
 setup.useHardware = useHardware;
 setup.origin = [0.0,0.0,0.0];
